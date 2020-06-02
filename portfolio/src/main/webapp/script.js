8ce9d799d25e2a26e089c41748bf9cbd53c38c56
Nnenna <nnennadara@gmail.com>
2020-06-01 23:51:24 -0500

    Changed DataServlet to my Name

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['I am 18 years old', 'My favorite color is purple.', 'I am a rising junior.', 'I love desserts.'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
